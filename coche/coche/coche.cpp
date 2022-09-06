fb4c461029ad5d927f170f1b4d17c2602ead73ac
Enr1que3 <99999296+Enr1que3@users.noreply.github.com>
2022-09-06 18:33:06 -0500

    Uso de la clase coche

---
+++ coche/coche/coche.cpp
@@ -0,0 +1,15 @@
+// coche.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+
+class coche {
+    int doors = 4;
+};
+
+
+int main(){
+    cout << "Hello World!\n";
+}
+
