36494e01b242108c5fbbaebc79ac6eaf73b5a5d4
Enr1que3 <99999296+Enr1que3@users.noreply.github.com>
2022-09-06 22:48:15 -0500

    Add files via upload
    uso de la clase coche

---
+++ intro-a-la-programacion/Main.java
@@ -0,0 +1,59 @@
+import java . io . *;
+
+public class Main{
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader bufEntrada = new BufferedReader(new InputStreamReader(System . in));
+
+        String respuesta;
+
+        int incremPuer;
+
+        incremPuer = 0;
+
+        Coche miCoche = new Coche(incremPuer);
+
+        do {
+
+            do {
+                System.out.print("agregar puerta al coche? (s/n): ");
+
+                respuesta = bufEntrada.readLine();
+
+                respuesta = respuesta.toLowerCase();
+
+            } while (!(respuesta.equals("s") || respuesta.equals("n")));
+
+            if (respuesta.equals("s")) {
+    
+                // crear objeto de clase coche
+                incremPuer=miCoche.puertas(incremPuer);
+                // Imprimir mensaje
+
+                System.out.println("Número de puertas que tiene el objeto: "+incremPuer);
+
+            }
+
+        } while (!respuesta.equals("n"));
+
+    }
+
+}
+
+    // clase Coche 
+
+class Coche {
+   int puertas;
+
+   Coche (int p) {
+
+        puertas=p;
+
+    }
+   int puertas(int incremPuer){
+
+        return incremPuer+1;
+
+    }
+
+}
